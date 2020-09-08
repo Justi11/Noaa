bb16fa96a6f5fb970dcecbe3f119b4e158b0c716
Justi11 <justynavarro@gmail.com>
2020-09-08 15:34:54 -0300

    En Boya Controller modifuque el metodo crearBoya: agregue un if y un badRequest.Hice la clase Muestra Controller con el metodo crearMuestra que falta terminar y obtenerMuestraPorColor.

--- src/main/java/ar/com/ada/api/noaa/controller/BoyaController.java
+++ src/main/java/ar/com/ada/api/noaa/controller/BoyaController.java
@@ -1,5 +1,8 @@
 package ar.com.ada.api.noaa.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,10 +14,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ar.com.ada.api.noaa.entity.Boya;
+import ar.com.ada.api.noaa.model.request.BoyaActualizarColorRequest;
 import ar.com.ada.api.noaa.model.request.BoyaRequest;
 import ar.com.ada.api.noaa.model.response.GenericResponse;
 import ar.com.ada.api.noaa.service.BoyaService;
-
 
 @RestController
 public class BoyaController {
@@ -22,25 +25,41 @@ public class BoyaController {
     @Autowired
     BoyaService boyaService;
 
-    @PostMapping("/api/boya")
+    @PostMapping("/api/boyas")
     public ResponseEntity<GenericResponse> crearBoya(@RequestBody BoyaRequest boyaRequest) {
-      Boya boya =  boyaService.crearBoya(boyaRequest.longitudInstalacion, boyaRequest.latitudInstalacion  );
-        GenericResponse r = new GenericResponse();
-        r.isOk = true;
-        r.message = "Boya Creada con exito";
-        r.id = boya.getIdBoya();
-        return ResponseEntity.ok(r);
+        Boya boya = boyaService.crearBoya(boyaRequest.longitudInstalacion, boyaRequest.latitudInstalacion);
+        if (boya != null) {
+
+            return ResponseEntity.ok(new GenericResponse(true, "Boya creada con exito", boya.getIdBoya()));
+        }
+        return ResponseEntity.badRequest().build();
 
     }
 
     @GetMapping("/api/boyas")
-    public ResponseEntity<?> listarBoyas() {
+    public ResponseEntity<List<Boya>> listarBoyas() {
         return ResponseEntity.ok(boyaService.listarBoyas());
     }
 
+    // hacer la validacion, chequear si la lista esta vacia/
     @GetMapping("/api/boyas/{id}")
-    public ResponseEntity<?> listarBoyaPorId(@PathVariable int id) {
-        return ResponseEntity.ok(boyaService.listarBoyasPorId(id));
+    public ResponseEntity<Boya> obtenerBoyaPorId(@PathVariable Integer id) {
+        Optional<Boya> boyaOp = boyaService.obtenerBoyaPorId(id);
+        if (boyaOp.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(boyaOp.get());
+
+    }
+
+    @PutMapping("/api/boyas/{id}")
+    public ResponseEntity<GenericResponse> actualizarColorBoya(@PathVariable Integer id,
+            @RequestBody BoyaActualizarColorRequest boyaActualizarColor) {
+        boolean boyaActualizada = boyaService.actualizarColorBoya(id, boyaActualizarColor.color);
+        if (boyaActualizada) {
+            return ResponseEntity.ok(new GenericResponse(true, "Boya actualizada", id));
+        }
+        return ResponseEntity.badRequest().build();
 
     }
 
